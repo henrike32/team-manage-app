2d35efafc709ef643690f67751b7801750bfc579
henrike32 <henrique.lambertucci@gmail.com>
2023-11-22 17:05:44 -0500

    using reat-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import Header from '/home/henrike32/team-manage-app/src/Header.tsx'
-import Employees from './Employees'
-import Footer from '/home/henrike32/team-manage-app/src/Footer.tsx'
 import './App.css'
+import Header from './Header'
+import Employees from './Employees'
+import Footer from './Footer'
+import GroupedTeamMembers from './GroupedTeamMembers'
 import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 
 function App() {
@@ -124,19 +126,27 @@ function App() {
   }
 
   return (
-    <div>
+    <Router>
       <Header
         selectedTeam={selectedTeam}
         teamMemberCount={employees.filter((employee) => employee.teamName === selectedTeam).length}
       />
-      <Employees
-        employees={employees}
-        selectedTeam={selectedTeam}
-        handleEmployeeCardClick={handleEmployeeCardClick}
-        handleTeamSelectionChange={handleTeamSelectionChange}
-      />
+      <Routes>
+        <Route path="/"
+          element={<Employees
+            employees={employees}
+            selectedTeam={selectedTeam}
+            handleEmployeeCardClick={handleEmployeeCardClick}
+            handleTeamSelectionChange={handleTeamSelectionChange}
+          />}>
+
+        </Route>
+        <Route parth="/GroupedTeamMembers" element={<GroupedTeamMembers/>}>
+
+        </Route>
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   )
 }
 
