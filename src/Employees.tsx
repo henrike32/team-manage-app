cefb8437f5091f73c74b676d6e179a18e2604d74
henrike32 <henrique.lambertucci@gmail.com>
2023-11-23 18:38:52 -0500

    adding teams

--- src/Employees.tsx
+++ src/Employees.tsx
@@ -1,40 +1,20 @@
-import femaleProfile from '/home/henrike32/team-manage-app/src/assets/images/femaleProfile.jpg'
-import maleProfile from '/home/henrike32/team-manage-app/src/assets/images/maleProfile.jpg'
+import Teams from './Teams'
+import TeamMembers from './TeamMembers'
 
-const Employees = ({ employees, selectedTeam, handleTeamSelectionChange, handleEmployeeCardClick }) => {
+const Employees = () => {
 
   return (
-
     <main className="container">
-      <div className="row justify-content-center mt-3 mb-3">
+      <div className="row justify-content-center">
         <div className="col-6">
-          { /* teams code */}
-          <select className="form-select form-select-lg" value={selectedTeam} onChange={handleTeamSelectionChange}>
-            <option value="TeamA">TeamA</option>
-            <option value="TeamB">TeamB</option>
-            <option value="TeamC">TeamC</option>
-            <option value="TeamD">TeamD</option>
-          </select>
+          <Teams />
         </div>
       </div>
-
-      <div className="row justify-content-center mt-3 mb-3">
+      <hr />
+      <div className="row justify-content-center">
         <div className="col-8">
           <div className="card-collection">
-            {
-              employees.map((employee) => (
-                <div key={employee.id} id={employees.id} className={(employee.teamName === selectedTeam ? 'card m-2 standout' : 'card m-2')} style={{ cursor: "pointer" }} onClick={handleEmployeeCardClick}>
-
-                  {(employee.gender === 'male') ? <img src={maleProfile} className="card-img-top" />
-                    : <img src={femaleProfile} className="card-img-top" />}
-
-                  <div className="card-group">
-                    <h5 className="card-title">Full Name: {employee.fullName}</h5>
-                    <p className="card-text"><b>Designation:</b>{employee.designation}</p>
-                  </div>
-                </div>
-              ))
-            }
+            <TeamMembers />
           </div>
         </div>
       </div>
