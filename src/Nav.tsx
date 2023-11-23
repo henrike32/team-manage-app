ccaa1beaffe18ef3fad3f2deb6ef4828396aa237
henrike32 <henrique.lambertucci@gmail.com>
2023-11-23 18:03:13 -0500

    navbar changes

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 const Nav = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <ul className="navbar-nav me-auto mb-lg-0">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
                     <Link className="nav-link" to="/">Home</Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" to="/GropupedTeamMembers">GroupedTeamMembers</Link>
+                    <Link className="nav-link" to="/GropupedTeamMembers"> Teams </Link>
 
                 </li>
             </ul>
