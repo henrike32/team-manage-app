d362a26fc766360c8f2106943e81468ae5d92916
henrike32 <henrique.lambertucci@gmail.com>
2023-11-23 18:16:44 -0500

    fixing state for groupedTeamMembers

--- src/GroupedTeamMembers.tsx
+++ src/GroupedTeamMembers.tsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 
 const GroupedTeamMembers = ({employees, selectedTeam, setTeam}) => {
 
-    const [GroupedTeamMembers, setGroupedData] = useState(GroupedTeamMembers());
+    const [GroupedTeamMembers, setGroupedData] = useState(groupTeamMembers());
 
-    function groupedTeamMembers()
+    function groupTeamMembers()
     {
      var teams = [];
 
@@ -28,31 +28,30 @@ const GroupedTeamMembers = ({employees, selectedTeam, setTeam}) => {
     }
 
 function handleTeamClick(event){
-        var newGroupData = goupedEmployees.map((groupedData) => groupedData.team === event.currentTarget.id ?{...groupedData, collapsed:!groupedData.collapsed}
-                                                                                                            :groupedData);
+        var transformedGroupData = groupedEmployees.map((groupedData) => groupedData.team === event.currentTarget.id ?{...groupedData, collapsed:!groupedData.collapsed}:groupedData);
+    
     setGroupedData(transformedGroupData);
     setTeam(event.currentTarget.id);
 }
-
 
     return (
         <main className="container">
         {
             groupedEmployees.map((item) => {
                 return (
-                    <div key ={item.team} className='card mt-2' style={{cursor: "pointer"}}>
+                    <div key ={item.team} className='card mt-2' style={{cursor:"pointer"}}>
                         <h4 id={item.team} className="card-header text-secondary bg-white" onClick={handleTeamClick}>
                             Team Name : {item.team}
                         </h4>
                         <div id={"collapse_" + item.team}
-                                className= {item.collapsed === true?"collapsed":""}>
+                                className={item.collapsed === true?"collapsed":""}>
                             <hr/>
                             {
                                 item.members.map(member => {
                                     return (
                                         <div className="mt-2">
                                             <h5 className="card-title mt-2">
-                                                <span className='text-dark'> Full Name : {member.fullName} </span>
+                                                <span className="text-dark"> Full Name : {member.fullName} </span>
                                             </h5>
                                             <p> Designation: {member.designation} </p>
                                         </div>
--- src/NotFound.tsx
+++ src/NotFound.tsx
@@ -1,13 +1,13 @@
 const NotFound = () => {
 
     return (
-        <main className="container">
+        <header className="container">
             <div className="row justify-content-center mt-3 mb-4">
                 <div className="col-8">
                     <h1 className="text-danger"> 404 - Page Not Found </h1>
                 </div>
             </div>
-        </main>
+        </header>
     )
 }
 export default NotFound;
