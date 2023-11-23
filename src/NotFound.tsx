14a6506d4267736b88500ad37434c169addb5e82
henrike32 <henrique.lambertucci@gmail.com>
2023-11-23 18:43:25 -0500

    adding header msg

--- src/NotFound.tsx
+++ src/NotFound.tsx
@@ -1,13 +1,12 @@
 const NotFound = () => {
-
     return (
-        <header className="container">
-            <div className="row justify-content-center mt-3 mb-4">
-                <div className="col-8">
-                    <h1 className="text-danger"> 404 - Page Not Found </h1>
-                </div>
-            </div>
-        </header>
+      <main className="container">
+        <div className="row justify-content-center mt-3 mb-3">
+          <div className="col-8">
+            <h1 className="text-danger">404 - Page not found</h1>
+          </div>
+        </div>
+      </main>
     )
-}
-export default NotFound;
+  }
+  export default NotFound
